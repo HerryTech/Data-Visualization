34cc8fc6fb14380e1eb887b36b2ed7f5172122fa
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-03-20 17:04:31 +1200

    Data Visualization

--- mpl.squares.py
+++ mpl.squares.py
@@ -7,5 +7,5 @@ ax.plot(squares, linewidth = 3)
 """Set plot title and label axes"""
 ax.set_title("Square Numbers", fontsize = 24)
 ax.set_xlabel("Value", fontsize = 14)
-ax.set_ylabel("Value", fontsize = 14)
+ax.set_ylabel("Square of Value", fontsize = 14)
 plt.show()
