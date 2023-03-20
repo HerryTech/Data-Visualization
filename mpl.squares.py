83923d8fa4eca5391b0e3373f097cd64ad596f3a
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-03-20 17:06:46 +1200

    Data Visualization

--- mpl.squares.py
+++ mpl.squares.py
@@ -4,8 +4,13 @@ squares = [1, 4, 9, 16, 25]
 
 fig, ax = plt.subplots()
 ax.plot(squares, linewidth = 3)
-"""Set plot title and label axes"""
+
+#Set plot title and label axes
 ax.set_title("Square Numbers", fontsize = 24)
 ax.set_xlabel("Value", fontsize = 14)
 ax.set_ylabel("Square of Value", fontsize = 14)
+
+#Set size of thick labels
+ax.tick_params(axis = "both", labelsize = 14)
+
 plt.show()
