f1a3d7a9ec5700e3d0c366156a06dcbce3a6fd61
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-03-23 17:26:19 +1200

    Data Visualization

--- mpl.squares.py
+++ mpl.squares.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 input_value = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
+#print(plt.style.available) 
+
 plt.style.use("seaborn")
 fig, ax = plt.subplots()
 ax.plot(input_value, squares, linewidth = 3)
@@ -14,5 +16,7 @@ ax.set_ylabel("Square of Value", fontsize = 14)
 
 #Set size of thick labels
 ax.tick_params(axis = "both", labelsize = 14)
+
+
 
 plt.show()
