9e8750607385ed2a6d5560d00f44ed7971452563
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-03-24 17:35:00 +1200

    Data Visualization

--- scatter_squares.py
+++ scatter_squares.py
@@ -2,10 +2,12 @@ import matplotlib.pyplot as plt
 
 plt.style.use("seaborn")
 fig, ax = plt.subplots()
-ax.scatter(2, 4)
+ax.scatter(2, 4, s = 200)
 
-ax.set_title("", fontsize = 24)
-ax.set_xlabel()
-ax.set_ylabel()
+ax.set_title("Square Numbers", fontsize = 24)
+ax.set_xlabel("Value", fontsize = 14)
+ax.set_ylabel("Square of values", fontsize = 14)
+
+ax.tick_params(axix =)
 
 plt.show()
