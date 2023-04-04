5d7ec79aa0a02bc85dbf029e0424e5ec908d6a79
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-04 18:04:20 +1200

    g

--- scatter_squares3.py
+++ scatter_squares3.py
@@ -4,3 +4,11 @@ x_values = range (1, 1001)
 y_values = [x ** 2 for x in x_values]
 
 plt.style.use("seaborn")
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values)
+
+ax.set_title("Square Numbers", fontsize = 24)
+ax.set_xlabel("Value", fontsize = 14)
+ax.set_ylabel("Square of values", fontsize = 14)
+
+plt.show()
