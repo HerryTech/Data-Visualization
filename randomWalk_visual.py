845c8ae63cc471a462ef5708fa7768a58cd234cb
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-25 22:54:33 +1200

    Data Visualization

--- randomWalk_visual.py
+++ randomWalk_visual.py
@@ -10,10 +10,15 @@ while True:
     fig, ax = plt.subplots()
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c = point_numbers, cmap = plt.cm.Blues, edgecolors = "none", s = 15)
+    #Emphasis the first and last point
+    ax.scatter(0, 0, c = "green", edgecolors = "none", s = 100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c= "red", edgecolors = "none", s = 100)
+    
+    
     ax.set_title("Random Walk", fontsize = 24)
     ax.set_xlabel("X-steps", fontsize = 14)
     ax.set_ylabel("Y-steps", fontsize = 14)
-   
+
     plt.savefig("random_walk.png")
     
     keep_walking = input("Do you want to take another walk? y/n: ")
