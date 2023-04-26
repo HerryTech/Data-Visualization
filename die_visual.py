1e345762f384ada86ba24dd84021c306b9d9e5fb
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-27 00:30:44 +1200

    Data Visualization

--- die_visual.py
+++ die_visual.py
@@ -1,6 +1,6 @@
-from die import Die
 from plotly.graph_objs import Bar, Layout
 from plotly import offline
+from die import Die
 
 die = Die()
 
@@ -16,4 +16,7 @@ for value in range(1, die.num_sides+1):
     
 x_values = list(range(1, die.num_sides+1))
 data = [Bar(x = x_values, y = frequencies)]
+
+x_axis_config = {"title" : "Result"}
+y_axis_config = {"title" : "Frequency of Result"}
     
