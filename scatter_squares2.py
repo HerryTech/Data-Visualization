142c259d4c6a2f031c042724ab7424fd365fba00
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-26 18:27:15 +1200

    Data Visualization

--- scatter_squares2.py
+++ scatter_squares2.py
@@ -5,7 +5,7 @@ y_values = [1, 4, 9, 16, 25]
 
 plt.style.use("seaborn")
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s = 100)
+ax.scatter(x_values, y_values, c = (0, 0.8, 0), s = 100)
 
 ax.set_title("Square Numbers", fontsize = 24)
 ax.set_xlabel("Value", fontsize = 14)
