5fbc482b3b53ac3c580e2bb3c396e9c427701eb7
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-26 22:07:42 +1200

    Data Visualization

--- randomWalk_visual.py
+++ randomWalk_visual.py
@@ -7,5 +7,8 @@ rw.fill_walk()
 plt.style.use("classic")
 fig, ax = plt.subplots()
 ax.scatter(rw.x_values, rw.y_values, s = 10)
+ax.set_title("Random Walk", fontsize = 24)
+ax.set_xlabel("X-steps", fontsize = 14)
+ax.set_ylabel("Y-steps", fontsize = 14)
 
 plt.savefig("random_walk.png")
--- random_walk.py
+++ random_walk.py
@@ -30,8 +30,5 @@ class RandomWalk():
             y = self.y_values[-1] + y_step
             
             self.x_values.append(x)
-            self.x_values.append(x)
+            self.y_values.append(y)
             
-            
-
-
