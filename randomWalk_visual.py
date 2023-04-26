9d546e44ad9021ffcc1f71618d4e119b28be0144
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-26 22:30:23 +1200

    Data Visualization

--- randomWalk_visual.py
+++ randomWalk_visual.py
@@ -1,14 +1,21 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-rw = RandomWalk()
-rw.fill_walk()
+#keep making new walks as long as the program is active
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
 
-plt.style.use("classic")
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, color = "brown", s = 10)
-ax.set_title("Random Walk", fontsize = 24)
-ax.set_xlabel("X-steps", fontsize = 14)
-ax.set_ylabel("Y-steps", fontsize = 14)
-
-plt.savefig("random_walk.png")
+    plt.style.use("classic")
+    fig, ax = plt.subplots()
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c = point_numbers, cmap = plt.cm.Blues, edgecolors = "none", s = 15)
+    ax.set_title("Random Walk", fontsize = 24)
+    ax.set_xlabel("X-steps", fontsize = 14)
+    ax.set_ylabel("Y-steps", fontsize = 14)
+   
+    plt.savefig("random_walk.png")
+    
+    keep_walking = input("Do you want to take another walk? y/n: ")
+    if keep_walking == "n":
+        break
