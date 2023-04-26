bf3861529d447d4243975505701bc7433823e7d5
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-26 21:06:13 +1200

    Data Visualization

---
+++ cubic_numbers.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+x_values = range(1, 5001)
+y_values = ([x ** 3 for x in x_values])
+
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, c = y_values, cmap = plt.cm.Blues, s = 10)
+
+ax.set_title("Cubic Numbers", fontsize = 24)
+ax.set_xlabel("Values", fontsize = 14)
+ax.set_ylabel("Cubic Values", fontsize = 14)
+ax.tick_params(axis = "both", labelsize = 14)
+
+plt.savefig("cubic_numbers.png")
