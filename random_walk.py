b4105906ced5b0679843c62a45bc953abac4f588
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-26 21:42:53 +1200

    Data visualization

---
+++ random_walk.py
@@ -0,0 +1,9 @@
+from random import choice
+
+class RandomWalk():
+    """A class to generate random walk"""
+    
+    def __init__(self, num_points = 5000):
+        """Initialize attributes of a walk."""
+        self.num_points = num_points
+        
