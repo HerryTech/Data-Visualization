0048dfd6cdcecbd78db52781f7c8642751036b3e
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-27 11:23:27 +1200

    Data Visualization

--- dice_visual.py
+++ dice_visual.py
@@ -12,14 +12,14 @@ for roll_num in range(1000):
     
 frequencies = []
 max_result = die_1.num_sides + die_2.num_sides
-for value in range(1, max_result+1):
+for value in range(2, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
     
-x_values = list(range(1, max_result+1))
+x_values = list(range(2, max_result+1))
 data = [Bar(x = x_values, y = frequencies)]
 
-x_axis_config = {"title" : "Result"}
+x_axis_config = {"title" : "Result", "dtick" : 1}
 y_axis_config = {"title" : "Frequency of Result"}
 my_layout = Layout(title = "Result of rolling two dice 1000 times", xaxis = x_axis_config, yaxis = y_axis_config)
 
