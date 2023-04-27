6db76e0306f0cde0792f27faca8d33c33bbd84fb
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-28 08:51:55 +1200

    Download data

--- Data/sitka_highs.py
+++ Data/sitka_highs.py
@@ -4,4 +4,6 @@ filename = "Data/sitka_weather.csv"
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
+    
+for index, column_header in enumerate(header_row):
+    print(index, column_header)
