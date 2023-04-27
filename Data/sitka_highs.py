d0ce4dca8c9c46e46ddcc22553962efe2d10fc15
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-28 11:59:43 +1200

    Data Visualization

--- Data/sitka_highs.py
+++ Data/sitka_highs.py
@@ -1,5 +1,6 @@
 import csv
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 filename = "Data/sitka_weather.csv"
 with open(filename) as f:
@@ -8,13 +9,16 @@ with open(filename) as f:
     
 #Get high temperature
     highs = []
+    date = []
     for row in reader:
+        current_date = datetime.strptime(row[2], "%Y-%m-%d")
         high = int(row[5])
+        date.append(current_date)
         highs.append(high)
     
 plt.style.use("seaborn")
 fig, ax = plt.subplots()
-ax.plot(highs, c = "red")
+ax.plot(date, highs, c = "red")
 
 plt.title("Daily high temperature, July 2018", fontsize = 24)
 plt.xlabel("", fontsize = 14)
