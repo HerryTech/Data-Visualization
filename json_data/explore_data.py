c5910b6997ec5e65945cd92550a92043fe9ae09e
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-28 13:53:49 +1200

    Data Visualization

--- json_data/explore_data.py
+++ json_data/explore_data.py
@@ -4,6 +4,9 @@ filename = "json_data/eq_1.json"
 with open(filename) as f:
     eq_1_data = json.load(f)
     
-filename = "json_data/readable_eq_1.json"
+"""filename = "json_data/readable_eq_1.json"
 with open(filename, "w") as f:
-    json.dump(eq_1_data, f, indent = 4)
+    json.dump(eq_1_data, f, indent = 4)"""
+    
+eq_dicts = eq_1_data["features"]
+print(eq_dicts)
