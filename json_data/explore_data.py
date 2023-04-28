0f2d04ef8d80c5bed6d36b1a462848ed6bcb584d
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-28 14:16:22 +1200

    Data Visualization

--- json_data/explore_data.py
+++ json_data/explore_data.py
@@ -10,9 +10,13 @@ with open(filename, "w") as f:
     
 eq_dicts = eq_1_data["features"]
 
-mags =[]
+mags, lons, lats = [], [], []
 for all in eq_dicts:
     mag = all["properties"]["mag"]
+    lon = all["geometry"]["coordinates"][0]
+    lat = all["geometry"]["coordinates"][1]
     mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
     
-print(mags[:10])
+print(lats)
