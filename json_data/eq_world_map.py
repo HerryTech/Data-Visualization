e67e8abd6ca7485aa52b5b9a165541425527f4e8
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-28 15:24:41 +1200

    Data Visualization

--- json_data/eq_world_map.py
+++ json_data/eq_world_map.py
@@ -8,21 +8,28 @@ with open(filename) as f:
 
 eq_dicts = eq_1_data["features"]
 
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_texts = [], [], [], []
 for all in eq_dicts:
     mag = all["properties"]["mag"]
     lon = all["geometry"]["coordinates"][0]
     lat = all["geometry"]["coordinates"][1]
+    title = all["properties"]["title"]
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    hover_texts.append(title)
 
 data = [{
     "type" : "scattergeo",
     "lon" : lons,
     "lat" : lats,
+    "text" : hover_texts,
     "marker" : {
-        "size" : [5 * mag for mag in mags]
+        "size" : [5 * mag for mag in mags],
+        "color" : mags,
+        "colorscale" : "Viridis",
+        "reversescale" : True,
+        "colorbar" : {"title" : "Magnitude"}
     },
 }]
 my_layout = Layout(title = "Global Earthquake")
