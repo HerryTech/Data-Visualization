8730527b0bdc7b9cedd8619625424249689044aa
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-28 13:29:21 +1200

    Data Visualization

---
+++ json_data/explore_data.py
@@ -0,0 +1,9 @@
+import json
+
+filename = "json_data/eq_1.json"
+with open(filename) as f:
+    eq_1_data = json.load(f)
+    
+filename = "json_data/readable_eq_1.json"
+with open(filename, "w") as f:
+    json.dump(eq_1_data, f, indent = 4)
