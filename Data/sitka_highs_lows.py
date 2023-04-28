c5c63048fef8743a488a84d16d27d679091c53f0
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-28 12:27:46 +1200

    Data Visualization

---
+++ Data/sitka_highs_lows.py
@@ -0,0 +1,33 @@
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+filename = "Data/sitka_weather_2018.csv"
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    
+#Get high temperature
+    highs = []
+    date = []
+    lows = []
+    for row in reader:
+        current_date = datetime.strptime(row[2], "%Y-%m-%d")
+        high = int(row[5])
+        low = int(row[6])
+        date.append(current_date)
+        highs.append(high)
+        lows.append(low)
+    
+plt.style.use("classic")
+fig, ax = plt.subplots()
+ax.plot(date, highs, c = "red")
+ax.plot(date, lows, c ="blue")
+
+plt.title("Daily high and low temperature, 2018", fontsize = 24)
+plt.xlabel("Date", fontsize = 14)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize = 16)
+plt.tick_params(axis = "both", which = "major", labelsize = 16)
+
+plt.savefig("Data/sitka_highs_lows.png")
