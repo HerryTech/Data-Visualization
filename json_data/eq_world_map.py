128cf8db604d35e134c6ab61f29913cf6e3d4b34
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-28 15:30:04 +1200

    Data Visualization

--- json_data/eq_world_map.py
+++ json_data/eq_world_map.py
@@ -32,6 +32,6 @@ data = [{
         "colorbar" : {"title" : "Magnitude"}
     },
 }]
-my_layout = Layout(title = "Global Earthquake")
+my_layout = Layout(title = eq_1_data["metadata"]["title"])
 fig = {"data" : data, "layout" : my_layout}
 offline.plot(fig, filename = "global_earthquake.html")
