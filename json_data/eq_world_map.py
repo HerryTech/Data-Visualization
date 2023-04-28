e2fd065317d7f5f2cc7deed96cecdcd2f441e0da
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-28 14:51:42 +1200

    Data Visualization

--- json_data/eq_world_map.py
+++ json_data/eq_world_map.py
@@ -2,7 +2,7 @@ import json
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
-filename = "json_data/eq_1.json"
+filename = "json_data/eq_30.json"
 with open(filename) as f:
     eq_1_data = json.load(f)
 
@@ -17,7 +17,14 @@ for all in eq_dicts:
     lons.append(lon)
     lats.append(lat)
 
-data = [Scattergeo(lon = lons, lat = lats)]
+data = [{
+    "type" : "scattergeo",
+    "lon" : lons,
+    "lat" : lats,
+    "marker" : {
+        "size" : [5 * mag for mag in mags]
+    },
+}]
 my_layout = Layout(title = "Global Earthquake")
 fig = {"data" : data, "layout" : my_layout}
 offline.plot(fig, filename = "global_earthquake.html")
